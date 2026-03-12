e06920bb7ae114ae62f03c63bef024a6388d9949
juergen2025sys <georg-schwaiger@gmx.de>
2026-03-12 20:16:50 +0100

    Delete firewall_exports directory

--- firewall_exports/blacklist_iptables.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a47e4d12fda75de7eb2eda84242e21d20542cfa7379d3de1822df66c5d468a4d
-size 140243364
--- firewall_exports/blacklist_mikrotik.rsc
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2261a7e492d2429658cdca936fd93ed8d5f5b028235ecf760bc957bd07ce540d
-size 288621359
